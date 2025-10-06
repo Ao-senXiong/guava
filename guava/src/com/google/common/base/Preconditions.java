9cfc27c1f05d051f661bc5007397ec2647cb2016
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-10-06 03:48:46 -0400

    keep Google's import

--- guava/src/com/google/common/base/Preconditions.java
+++ guava/src/com/google/common/base/Preconditions.java
@@ -18,7 +18,7 @@ import static com.google.common.base.Strings.lenientFormat;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
-
+import java.util.logging.Logger;
 import javax.annotation.CheckForNull;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.nullness.qual.Nullable;
--- guava/src/com/google/common/collect/SingletonImmutableSet.java
+++ guava/src/com/google/common/collect/SingletonImmutableSet.java
@@ -18,7 +18,7 @@ package com.google.common.collect;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.base.Preconditions;
-
+import com.google.errorprone.annotations.concurrent.LazyInit;
 import javax.annotation.CheckForNull;
 import org.checkerframework.checker.index.qual.NonNegative;
 import org.checkerframework.checker.signedness.qual.UnknownSignedness;
